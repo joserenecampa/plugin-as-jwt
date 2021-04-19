4846a40ffff5764e13e2fe492d7a2297c81f2f93
09275643784 <jose.campanario@serpro.gov.br>
2021-04-19 09:20:29 -0300

    versao com driver pkcs11

--- plugin/src/main/java/br/gov/serpro/JWTIssuer.java
+++ plugin/src/main/java/br/gov/serpro/JWTIssuer.java
@@ -1,16 +1,18 @@
 package br.gov.serpro;
 
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.security.KeyStore;
 import java.security.PrivateKey;
+import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.Iterator;
+import java.util.Map.Entry;
 
 import org.apache.commons.codec.binary.Base64;
 import org.demoiselle.signer.core.extension.BasicCertificate;
 import org.demoiselle.signer.core.keystore.loader.KeyStoreLoader;
+import org.demoiselle.signer.core.keystore.loader.configuration.Configuration;
 import org.demoiselle.signer.core.keystore.loader.factory.KeyStoreLoaderFactory;
 import org.demoiselle.signer.cryptography.Digest;
 import org.demoiselle.signer.cryptography.DigestAlgorithmEnum;
@@ -24,7 +26,10 @@ import org.demoiselle.signer.serpro.desktop.command.cert.Certificate;
 import org.demoiselle.signer.serpro.desktop.command.cert.ListCerts;
 import org.demoiselle.signer.serpro.desktop.command.cert.ListCertsRequest;
 import org.demoiselle.signer.serpro.desktop.command.cert.ListCertsResponse;
+import org.demoiselle.signer.serpro.desktop.exception.ActionCanceledException;
+import org.demoiselle.signer.serpro.desktop.pkcs11info.KeystoreSelection;
 import org.demoiselle.signer.serpro.desktop.ui.ListCertificateData;
+import org.demoiselle.signer.serpro.desktop.ui.PinHandler;
 import org.demoiselle.signer.serpro.desktop.ui.PinHandlerFile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -36,38 +41,33 @@ public class JWTIssuer extends AbstractCommand<JWTRequest, JWTResponse> {
     private Configurations configSigner = Configurations.getInstance();
     private JWTResponse response = new JWTResponse();
     private char[] pass = null;
-    private static final String ACAO = "Emitir Web Token";
+    private static String ACAO = "Emitir Web Token";
 
     @Override
     public JWTResponse doCommand(JWTRequest request) throws Throwable {
         try {
+            if (request.getHostConnected() != null && !request.getHostConnected().isEmpty()) {
+                JWTIssuer.ACAO = JWTIssuer.ACAO + " - " + request.getHostConnected();
+            }
             String alias = this.getAlias();
-			X509Certificate cert = (X509Certificate) keyStore.getCertificate(alias);
-			BasicCertificate bc = new BasicCertificate(cert);
+            X509Certificate cert = (X509Certificate) keyStore.getCertificate(alias);
+            BasicCertificate bc = new BasicCertificate(cert);
             Digest digest = DigestFactory.getInstance().factory();
             digest.setAlgorithm(DigestAlgorithmEnum.SHA_1);
             String certSha1 = base64Codec(digest.digest(cert.getEncoded()));
             long now = System.currentTimeMillis() / 1000L;
-            String headerJwt = "{"+
-                            "\"alg\":\"RS512\","+
-                            "\"typ\":\"JWT\","+
-                            "\"x5t\":\"" + certSha1 + "\""+
-                            "}";
-            String bodyJwt = "{"+
-                             "\"iss\":\"Assinador SERPRO Websocket Service\"," +
-                             "\"iat\":" + now +  "," +
-                             "\"nbf\":" + now +  "," +
-                             "\"exp\":" + (now+3600) +  "," +
-                             "\"prn\":\"" + bc.getICPBRCertificatePF().getCPF() + "\","+
-                             ""+(request.isWithCert()?"\"crt\":\"" + base64Codec(cert.getEncoded()) + "\",":"")+
-                             "\"sub\":\"" + bc.getName() + "\""+
-                             "}";
+            String headerJwt = "{" + "\"alg\":\"RS512\"," + "\"typ\":\"JWT\"," + "\"x5t\":\"" + certSha1 + "\"" + "}";
+            String bodyJwt = "{" + "\"iss\":\"Assinador SERPRO Websocket Service\"," + "\"iat\":" + now + ","
+                    + "\"nbf\":" + now + "," + "\"exp\":" + (now + 3600) + "," + "\"prn\":\""
+                    + bc.getICPBRCertificatePF().getCPF() + "\"," + ""
+                    + (request.isWithCert() ? "\"crt\":\"" + base64Codec(cert.getEncoded()) + "\"," : "") + "\"sub\":\""
+                    + bc.getName() + "\"," + "\"sys\":\"" + request.getHostConnected() + "\"" + "}";
             PKCS1Signer signer = PKCS1Factory.getInstance().factory();
             signer.setAlgorithm(SignerAlgorithmEnum.SHA512withRSA);
-            signer.setPrivateKey((PrivateKey)this.keyStore.getKey(alias, this.pass));
-            String toSigner = base64Codec(headerJwt.getBytes())+"."+base64Codec(bodyJwt.getBytes());
+            signer.setPrivateKey((PrivateKey) this.keyStore.getKey(alias, this.pass));
+            String toSigner = base64Codec(headerJwt.getBytes()) + "." + base64Codec(bodyJwt.getBytes());
             byte[] signatureJwt = signer.doDetachedSign(toSigner.getBytes());
-            response.setJwt(toSigner+"."+base64Codec(signatureJwt));
+            response.setJwt(toSigner + "." + base64Codec(signatureJwt));
             response.setCertificate(base64Codec(cert.getEncoded()));
         } catch (Throwable error) {
             logger.error("Erro ao assinar token", error);
@@ -99,6 +99,60 @@ public class JWTIssuer extends AbstractCommand<JWTRequest, JWTResponse> {
             this.keyStore = loader.getKeyStore(String.copyValueOf(this.pass));
         } catch (Throwable error) {
             logger.error("Erro ao carregar KeyStore", error);
+        }
+
+        if (configSigner.isUseCertificateFile() || configSigner.isChooseFileCertificate()) {
+            try {
+                File filep12 = new File(configSigner.getCertificateFilePath());
+                if (configSigner.isSaveCertificateFilePass()) {
+                    this.pass = configSigner.getCertificateFilePass();
+                } else {
+                    PinHandlerFile pinFile = new PinHandlerFile(JWTIssuer.ACAO, null);
+                    pinFile.init();
+                    if (!pinFile.getActionCanceled()) {
+                        this.pass = pinFile.getPwd();
+                    } else {
+                        throw new RuntimeException("Cancelada pelo usuário");
+                    }
+                }
+                configSigner.setLastDriverUsed(filep12.getAbsolutePath());
+                KeyStoreLoader loader = KeyStoreLoaderFactory.factoryKeyStoreLoader(filep12);
+                this.keyStore = loader.getKeyStore(String.copyValueOf(this.pass));
+            } catch (Throwable error) {
+                logger.error("Erro ao carregar KeyStore", error);
+            }
+        } else {
+            PinHandler pinToken = new PinHandler(JWTIssuer.ACAO, "");
+            if (configSigner.isUseNeoId()) {
+                Iterator<Entry<String, String>> it = Configuration.getInstance().getDrivers().entrySet().iterator();
+                while (it.hasNext()) {
+                    Entry<String, String> item = it.next();
+                    if (!item.getKey().contains("neoid")) {
+                        it.remove();
+                    }
+                }
+            }
+            if (configSigner.isSelectedToken()) {
+                Iterator<Entry<String, String>> it = Configuration.getInstance().getDrivers().entrySet().iterator();
+                while (it.hasNext()) {
+                    Entry<String, String> item = it.next();
+                    if (!item.getKey().equalsIgnoreCase(configSigner.getTokenNameSelected())) {
+                        it.remove();
+                    }
+                }
+            }
+            try {
+                KeystoreSelection ks = new KeystoreSelection(pinToken, keyStore, null);
+                this.keyStore = ks.getKeyStore();
+                configSigner.setLastDriverUsed(ks.getLastUsedDriver());
+                if (this.keyStore == null) {
+                    response.setActionCanceled(true);
+                }
+            } catch (ActionCanceledException cancel) {
+                this.keyStore = null;
+            } catch (CertificateException error) {
+                this.keyStore = null;
+            }
         }
     }
 
